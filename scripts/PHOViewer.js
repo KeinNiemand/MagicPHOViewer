5ebbc5ac2bb5a1caa9c80e3265d52f55dbd0bb07
KeinNiemand <KeinNiemand@users.noreply.github.com>
2021-10-21 11:38:22 +0200

    find PHO topics with name, board and originalPoster

---
+++ scripts/PHOViewer.js
@@ -0,0 +1,51 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "Magic PHO Viewer Loaded" );
+    //$("article.js-selectToQuote").css("color","green")
+    topics = GetPHOTopics();
+    //Turn topics red for testing purposes
+    topics.each((i, element) => element.topicName.css("color", "red"))
+});
+
+function GetPHOTopics() {
+    let post = $("article.js-selectToQuote")
+    let bold = post.find($("b"));
+    //Get PHO Topics
+    let topics = bold.filter(function() {return this.innerHTML.match("♦ Topic:.*")});
+    //Wrap topic Names
+    topics.contents().filter((index,node) => node.textContent.match("♦ Topic:.*") && node.nodeType == 3).wrap('<span class="PHOTopicName">');
+    //Wrap topic Board
+    topics.contents().filter((index,node) => node.textContent.match("In: Boards ►.*") && node.nodeType == 3).wrap('<span class="PHOTopicBoards">');
+    //Wrap topic originalPoster
+    topics.contents().filter((index,node) => node.textContent.match(".*") && node.nodeType == 3).wrap('<span class="PHOTopicOp">');
+
+    //GetTopicName
+    /*** let topicName = $(".PHOTopicName");
+    let board = $(".PHOTopicBoards");
+    let originalPoster = $(".PHOTopicOp");
+    //Turn topic name blue for test purposes
+    topicName.css("color", "blue");
+    board.css("color", "red");
+    originalPoster.css("color", "green") */
+
+    pHOTopics = topics.each(() => true).map((index, element) => new PHOTopic(element))
+    return pHOTopics
+}
+
+class PHOTopic {
+
+    topicHeader;
+    topicName;
+    board;
+    originalPoster;
+
+    constructor(topic) {
+        this.topicHeader = $(topic);
+        //GetTopicName
+        this.topicName = this.topicHeader.find($(".PHOTopicName"));
+        //Get Board
+        this.board = this.topicHeader.find($(".PHOTopicBoards"));
+        //Get orignalPoster
+        this.originalPoster = this.topicHeader.find($(".PHOTopicOp"));
+    }
+}
