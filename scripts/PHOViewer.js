a739ae25a29d06e0d49dc623dd09dcf3b5d5bb2e
KeinNiemand <KeinNiemand@users.noreply.github.com>
2021-10-21 14:17:47 +0200

    started addiing posts

--- scripts/PHOViewer.js
+++ scripts/PHOViewer.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
     //$("article.js-selectToQuote").css("color","green")
     topics = GetPHOTopics();
     //Turn topics red for testing purposes
-    topics.each((i, element) => element.topicName.css("color", "red"))
+    topics.each((i, element) => element.topicNameElement.css("color", "red"))
 });
 
 function GetPHOTopics() {
@@ -22,6 +22,8 @@ function GetPHOTopics() {
     topics.contents().filter((index,node) => node.nodeType == 3).wrap('<span class="PHOTopicOp">');
     //Wrap All Lines
     post.children().contents().filter((index,node) => node.nodeType == 3).wrap('<span class="line">');
+
+
     let pHOTopics = topics.each(() => true).map((index, element) => new PHOTopic(element))
     return pHOTopics
 }
@@ -29,24 +31,59 @@ function GetPHOTopics() {
 class PHOTopic {
 
     topicHeader;
+    topicNameElement;
     topicName;
+    boardElement;
     board;
+    originalPosterElement;
     originalPoster;
+    originalPosterBadgesElement;
     originalPosterBadges;
+    postedOnElement;
     postedOn;
+    endElement;
 
     constructor(topic) {
         this.topicHeader = $(topic);
         //GetTopicName
-        this.topicName = this.topicHeader.find($(".PHOTopicName"));
+        this.topicNameElement = this.topicHeader.find($(".PHOTopicName"));
+        this.topicName = this.topicNameElement.text();
         //Get Board
-        this.board = this.topicHeader.find($(".PHOTopicBoards"));
+        this.boardElement = this.topicHeader.find($(".PHOTopicBoards"));
+        this.board = this.boardElement.text();
         //Get orignalPoster
-        this.originalPoster = this.topicHeader.find($(".PHOTopicOp"));
+        this.originalPosterElement = this.topicHeader.find($(".PHOTopicOp"));
+        this.originalPoster = this.originalPosterElement.text();
         //Get Badges
-        this.userBadges = this.topicHeader.next();
+        this.originalPosterBadgesElement = this.topicHeader.next();
+        this.originalPosterBadges = this.originalPosterBadgesElement.text();
         //Get PostedOn
-        this.postedOn = this.topicHeader.nextAll().filter((i,node) => node.textContent.match("Posted On.*$"))
-        .first();
+        this.postedOnElement = this.topicHeader.nextAll().filter((i,node) => node.textContent.match("Posted On.*$")).first()
+        this.postedOn = this.postedOnElement.text();
+        //Get End
+        this.endElement = this.topicHeader.nextAll().filter((i,node) => node.textContent.match("■.*$")).first();
+    }
+
+    getPosts() {
+        let topicContent = this.postedOnElement.nextUntil(this.endElement)
+        let bold = topicContent.find($("b"));
+        let postHeaders = bold.filter(function() {return this.innerHTML.match("►.*")});
+        return postHeaders.each(() => true).map((index, element) => new PHOpost(element))
+    }
+}
+
+class PHOpost {
+    postHeader;
+    userNameElement;
+    userBadgesElement;
+    postedOnElement;
+    postContentElement;
+    end;
+
+    constructor(postHeader) {
+        this.postHeader = $(postHeader)
+        this.userNameElement = this.postHeader;
+        this.userBadgesElement = this.postHeader.parent().filter((idx, element) => element.textContent.match("\(.*\)"));
+        this.postedOnElement = this.postHeader.parent().nextAll().filter((idx, element) => element.textContent.match(".*Replied On.*$")).first();
     }
 }
