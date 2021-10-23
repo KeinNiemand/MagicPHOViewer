9706dd4a69c1e2b50ee29b8d0a447a739fddb112
Kein Niemand <18308201+KeinNiemand@users.noreply.github.com>
2021-10-23 16:27:55 +0200

    added dev version of loader script

---
+++ MagicPHOViewerLoaderDev.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         MagicPHOViewerLoaderDev
+// @version      masterGithub
+// @description  Loads Magic PHO Viewer
+// @author       KeinNiemand
+// @match        https://forums.sufficientvelocity.com/threads/*
+// @icon         https://www.google.com/s2/favicons?domain=sufficientvelocity.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    $("body").append("<script src='https://raw.githack.com/KeinNiemand/MagicPHOViewer/master/scripts/PHOTopic.js' crossorigin='anonymous'><\/script>")
+    $("body").append("<script src='https://raw.githack.com/KeinNiemand/MagicPHOViewer/master/scripts/PHOpost.js' crossorigin='anonymous'><\/script>")
+    $("body").append("<script src='https://raw.githack.com/KeinNiemand/MagicPHOViewer/master/scripts/PHOViewer.js' crossorigin='anonymous'><\/script>")
+})();
