6a2181d1611a3f108a8b93623e66f8dea446b868
KeinNiemand <KeinNiemand@users.noreply.github.com>
2021-10-26 12:01:27 +0200

    updated main loader script

--- MagicPHOViewerLoader.user.js
+++ MagicPHOViewerLoader.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         MagicPHOViewerLoader
-// @version      0.4.1
+// @version      0.5.0
 // @description  Loads Magic PHO Viewer
 // @author       KeinNiemand
-// @match        https://forums.sufficientvelocity.com/threads/*
+// @inculde      https://forums.sufficientvelocity.com/threads/*
+// @inculde      https://forums.spacebattles.com//threads/*
 // @icon         https://www.google.com/s2/favicons?domain=sufficientvelocity.com
 // @grant        none
 // ==/UserScript==
@@ -12,7 +13,7 @@
     'use strict';
 
     // Your code here...
-    $("body").append("<script src='https://rawcdn.githack.com/KeinNiemand/MagicPHOViewer/0.4.1/scripts/PHOTopic.js' crossorigin='anonymous'><\/script>")
-    $("body").append("<script src='https://rawcdn.githack.com/KeinNiemand/MagicPHOViewer/0.4.1/scripts/PHOpost.js' crossorigin='anonymous'><\/script>")
-    $("body").append("<script src='https://rawcdn.githack.com/KeinNiemand/MagicPHOViewer/0.4.1/scripts/PHOViewer.js' crossorigin='anonymous'><\/script>")
+    $("body").append("<script src='https://rawcdn.githack.com/KeinNiemand/MagicPHOViewer/0.5.0/scripts/PHOTopic.js' crossorigin='anonymous'><\/script>")
+    $("body").append("<script src='https://rawcdn.githack.com/KeinNiemand/MagicPHOViewer/0.5.0/scripts/PHOpost.js' crossorigin='anonymous'><\/script>")
+    $("body").append("<script src='https://rawcdn.githack.com/KeinNiemand/MagicPHOViewer/0.5.0/scripts/PHOViewer.js' crossorigin='anonymous'><\/script>")
 })();
